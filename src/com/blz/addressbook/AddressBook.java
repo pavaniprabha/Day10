3a6921e2fee06ceb86bee0d602a5e90ec612e3fa
RAGOLUpavani <ragolupavani0330@gmail.com>
2021-09-23 07:34:53 +0530

    Adding Multiple contacts in AddressBook

---
+++ src/com/blz/addressbook/AddressBook.java
@@ -0,0 +1,10 @@
+package com.blz.addressbook;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System....!");
+		Contact contact = new Contact();
+		contact.Operations();
+	}
+}
